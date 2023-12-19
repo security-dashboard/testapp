a4759a78fc888caee1dc682df4a23cc79445a979
security-dashboard <secdashboard2@gmail.com>
2023-12-19 13:23:27 +0100

    Added an overcomplicated method

--- SomeClass.cs
+++ SomeClass.cs
@@ -82,5 +82,42 @@ namespace poc1
             var myBinaryFormatter = new BinaryFormatter();
             myBinaryFormatter.Deserialize(stream);
         }
+
+        public int OverComplicatedMethod(int value)
+        {
+            if(value == 0)
+            {
+                return 0;
+            }
+            if (value == 1)
+            {
+                return 1;
+            }
+            if (value == 2)
+            {
+                return 2;
+            }
+            if (value == 3)
+            {
+                return 3;
+            }
+            if (value == 4)
+            {
+                return 4;
+            }
+            if (value == 5)
+            {
+                return 5;
+            }
+            if (value == 6)
+            {
+                return 6;
+            }
+            if (value == 7)
+            {
+                return 7;
+            }
+            return value;
+        } 
     }
 }
