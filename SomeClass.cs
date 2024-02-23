5b042055e56e31b8a733b2ea470271d507cfb861
Christian Rudfoss <christian.rudfoss@soprasteria.com>
2024-02-23 11:06:41 +0100

    c

--- SomeClass.cs
+++ SomeClass.cs
@@ -32,7 +32,7 @@ namespace poc1
             var a = 0; 
             var b = 0;
             var c = 0;   
-            var d = 0;
+            var d = 0; 
             var e = 0; 
             var f = 0;   
             var g = 0; 
